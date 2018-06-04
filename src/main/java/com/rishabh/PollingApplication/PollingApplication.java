b822b0406fc4c18ef9c6158b95ca3a004f84c0b6
Rishabh Sharma <rishabhsharma@ufl.edu>
2018-06-04 11:40:10 -0500

    Add project with security config

---
+++ src/main/java/com/rishabh/PollingApplication/PollingApplication.java
@@ -0,0 +1,12 @@
+package com.rishabh.PollingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingApplication.class, args);
+	}
+}
---
+++ src/main/java/com/rishabh/PollingApplication/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,4 @@
+package com.rishabh.PollingApplication.security;
+
+public class JwtAuthenticationEntryPoint {
+}
