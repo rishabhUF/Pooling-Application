2f7af849f5b672d676bc65a5514fb3584a804a45
Rishabh Sharma <rishabhsharma@ufl.edu>
2018-06-04 16:21:01 -0500

    Add payloads for poll,vote and API's

---
+++ src/main/java/com/rishabh/PollingApplication/doa/PollRepository.java
@@ -0,0 +1,22 @@
+package com.rishabh.PollingApplication.doa;
+
+import com.rishabh.PollingApplication.model.Poll;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PollRepository extends MongoRepository<Poll,String> {
+    Optional<Poll> findById(String pollId);
+
+    Page<Poll> findByCreatedBy(String userId, Pageable pageable);
+
+    String countByCreatedBy(String userId);
+
+    List<Poll> findByIdIn(List<String> pollIds);
+
+    List<Poll> findByIdIn(List<String> pollIds, Sort sort);
+}
---
+++ src/main/java/com/rishabh/PollingApplication/doa/VoteRepository.java
@@ -0,0 +1,33 @@
+package com.rishabh.PollingApplication.doa;
+
+import com.rishabh.PollingApplication.model.ChoiceVoteCount;
+import com.rishabh.PollingApplication.model.Vote;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface VoteRepository extends MongoRepository<Vote,String> {
+
+    @Query("SELECT NEW ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id in :pollIds GROUP BY v.choice.id")
+    List<ChoiceVoteCount> countByPollIdInGroupByChoiceId(@Param("pollIds") List<Long> pollIds);
+
+    @Query("SELECT NEW ChoiceVoteCount(v.choice.id, count(v.id)) FROM Vote v WHERE v.poll.id = :pollId GROUP BY v.choice.id")
+    List<ChoiceVoteCount> countByPollIdGroupByChoiceId(@Param("pollId") Long pollId);
+
+    @Query("SELECT v FROM Vote v where v.user.id = :userId and v.poll.id in :pollIds")
+    List<Vote> findByUserIdAndPollIdIn(@Param("userId") Long userId, @Param("pollIds") List<Long> pollIds);
+
+    @Query("SELECT v FROM Vote v where v.user.id = :userId and v.poll.id = :pollId")
+    Vote findByUserIdAndPollId(@Param("userId") Long userId, @Param("pollId") Long pollId);
+
+    @Query("SELECT COUNT(v.id) from Vote v where v.user.id = :userId")
+    long countByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT v.poll.id FROM Vote v WHERE v.user.id = :userId")
+    Page<Long> findVotedPollIdsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+}
