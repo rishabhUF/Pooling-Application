c7424b92a8ef2613d4ccc42acbaae797b9c89e82
Rishabh Sharma <rishabhsharma@ufl.edu>
2018-06-05 11:52:47 -0500

    Update Poll model

--- src/main/java/com/rishabh/PollingApplication/model/Poll.java
+++ src/main/java/com/rishabh/PollingApplication/model/Poll.java
@@ -70,4 +70,12 @@ public class Poll {
     public void setCreatedOn(Date createdOn) {
         this.createdOn = createdOn;
     }
+
+    public List<Choice> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
+    }
 }
